ebc0d47ce24cf3bf8dabe6fdf87dea5306c8c3a8
SahanWickramage <sahanwickramage.16@cse.mrt.ac.lk>
2021-01-05 09:52:24 +0530

    updated passenger_count.py

--- experiments/passenger_count.py
+++ experiments/passenger_count.py
@@ -1,4 +1,6 @@
-# import dependencies
+##################################################
+##  import dependencies and define parameters   ##
+##################################################
 import os
 from gluoncv import model_zoo, data
 import mxnet as mx
@@ -6,7 +8,6 @@ import numpy as np
 import cv2
 from math import sqrt
 
-# define parameters
 threshold = 0.4
 green = (0, 255, 0)
 red = (0, 0, 255)
@@ -15,21 +16,20 @@ thickness = 2  # 2 pixel
 radius = 3
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
-video_path = "test.mp4"
+video_path = "test5.mp4"
 
-# define functions
+#########################
+##  define functions   ##
+#########################
+# extract predictions that above the threshold
 def predict(frame):
     bboxes_list = []
     scores_list = []
     box_ids_list = []
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     xrgb = mx.nd.array(rgb).astype('uint8')
-    # x, orig_img = data.transforms.presets.rcnn.load_test(im_fname)
     rgb_nd, xrgb = data.transforms.presets.rcnn.transform_test(xrgb, short=512, max_size=700)
     box_ids, scores, bboxes = model(rgb_nd.as_in_context(mx.gpu(0)))
-
-    # img = gcv.utils.viz.cv_plot_bbox(frame, bboxes[0], scores[0], box_ids[0], class_names=net.classes)
-    # gcv.utils.viz.cv_plot_image(img)
 
     bboxes = bboxes[0].asnumpy()
     scores = scores[0].asnumpy()
@@ -47,6 +47,7 @@ def predict(frame):
 
     return (bboxes_list, scores_list, box_ids_list)
 
+# extract predictions of person class
 def get_human_box_detection(boxes, scores, classes):
     array_boxes = list()
     for i in range(len(boxes)):
@@ -54,11 +55,8 @@ def get_human_box_detection(boxes, scores, classes):
             array_boxes.append((int(boxes[i][0]), int(boxes[i][1]), int(boxes[i][2]), int(boxes[i][3])))
     return array_boxes
 
-# select the model
-model = model_zoo.get_model('ssd_512_resnet50_v1_coco', pretrained=True, ctx=mx.gpu(0))
-fourcc1 = cv2.VideoWriter_fourcc(*"MJPG")
-output_video_1 = cv2.VideoWriter("video.avi", fourcc1, 20, (640, 480), True)
 
+# check near or not using euclidean distance
 def is_near(new_point, old_point, check_distance=50):
     real_distance = int(sqrt((new_point[0] - old_point[0])**2 + (new_point[1] - old_point[1])**2))
     if real_distance <= check_distance:
@@ -66,11 +64,26 @@ def is_near(new_point, old_point, check_distance=50):
     else:
         return False
 
-# init
+######################
+##  main program    ##
+######################
+# init step
+model = model_zoo.get_model('ssd_512_resnet50_v1_coco', pretrained=True, ctx=mx.gpu(0))
+fourcc1 = cv2.VideoWriter_fourcc(*"MJPG")
+output_video_1 = cv2.VideoWriter("video.avi", fourcc1, 20, (640, 480), True)
+
 vs = cv2.VideoCapture(video_path)
 count = 0
 passengers = []
+
 # main loop
+# program keeps a list 'passengers' for entire loop
+# list 'passengers' keeps the latest coordinates of centroids of passengers
+# number of elements in the list 'passengers' is the number of passengers in the bus
+# each iteration program creates list 'centroids' for current frame
+# list 'centroids' has the coordinates of centroids of detected passengers in the current frame
+# each iteration program updates the latest coordinates of centroids in list 'passengers' with help of 'is_near()' function
+# if there is new centroid in the list 'centroids', new centroid will append to list 'passengers'
 while True:
     (frame_exists, frame) = vs.read()
     key = cv2.waitKey(1) & 0xFF
@@ -80,27 +93,26 @@ while True:
         if key == ord('q'):
             break
         (boxes, scores, classes) = predict(frame)
+        # create list 'centroids'
         centroids = []
-        line = ""
         array_boxes_detected = get_human_box_detection(boxes, scores, classes)
         for index, downoid in enumerate(array_boxes_detected):
             xmin, ymin, xmax, ymax = [int(x) for x in array_boxes_detected[index]]
             xc = int((xmin + xmax) / 2)
             yc = int((ymin + ymax) / 2)
             centroids.append((xc, yc))
-            if line == "":
-                line = "%u,%u" % (xc, yc)
-            else:
-                line = "%s %u,%u" % (line, xc, yc)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), green, thickness)
             cv2.circle(frame, (xc, yc), radius, red, -1)
 
+        # no detected passengers in current frame
         if len(centroids) == 0:
             pass
         else:
+            # list 'passengers' is empty
             if len(passengers) == 0:
                 for i in range(len(centroids)):
                     passengers.append(centroids[i])
+            # update the latest coordinates of centroids of passengers
             else:
                 _passengers = passengers[:]
 
@@ -111,13 +123,13 @@ while True:
                             _passengers[j] = centroids[i]
                             updated += 1
 
+                    # new centroid found
                     if updated == 0:
                         _passengers.append(centroids[i])
 
                 passengers = _passengers[:]
         if len(passengers) > count:
             count = len(passengers)
-            print(count)
         t = "passengers : %u" % count
         cv2.putText(frame, t, (0, 90), font, fontScale, blue, thickness, cv2.LINE_AA)
         cv2.imshow("output", frame)
